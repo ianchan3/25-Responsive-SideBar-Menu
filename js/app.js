c5d3cedffe0c4b4e4ef64d5b50224fe48fa533d1
ianchan3 <ianchan3138@gmail.com>
2023-02-20 19:57:58 -0800

    Day 25 of 100 Days of JavaScript Completed

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ function smoothScroll(e) {
   closeMenu();
 }
 
+const menuWrapper = document.querySelector(".nav-wrapper");
 const menu = document.querySelector(".nav-list");
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
@@ -21,15 +22,18 @@ const close = document.querySelector(".close");
 const showMenu = () => {
   hamburger.style.display = "none";
   close.style.transform = "translateY(0)";
-  menu.style.transform = "translateY(0)";
+  menuWrapper.style.transform = "translateX(0)";
+  menu.style.transform = "translateX(0)";
 }
 
 const closeMenu = () => {
   close.style.transform = "translateY(-20rem)";
   hamburger.style.display = "block";
-  menu.style.transform = "translateY(-200%)";
+  menuWrapper.style.transform = "translateX(-200%)";
+  menu.style.transform = "translateX(200%)";
+
 }
 
 hamburger.addEventListener("click", showMenu);
 close.addEventListener("click", closeMenu);
-
+menuWrapper.addEventListener("click", closeMenu)
